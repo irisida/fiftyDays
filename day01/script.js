bdeb5a8474ede62ee16e33f233702c996ff52d25
irisida <20445648+irisida@users.noreply.github.com>
2021-01-27 21:07:18 +0000

    added day01

---
+++ day01/script.js
@@ -0,0 +1,19 @@
+const panels = document.querySelectorAll('.panel');
+
+panels.forEach((panel) => {
+    panel.addEventListener('click', () => {
+        removeActiveClass();
+        panel.classList.add('active');
+    });
+
+    panel.addEventListener('mouseover', () => {
+        removeActiveClass();
+        panel.classList.add('active');
+    });
+});
+
+function removeActiveClass() {
+    panels.forEach((panel) => {
+        panel.classList.remove('active');
+    });
+}
